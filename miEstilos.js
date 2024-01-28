a8bf7886d825fea79a7fa33ab4a3cc1eab1ac3d8
Miguel-Briceno <bricenomigueles@gmail.com>
2024-01-28 16:01:42 +0100

    primer commit

---
+++ miEstilos.js
@@ -0,0 +1,8 @@
+    // funcion que se ejecuta cuando se produce el llamado a la funcion en el DOM
+    // funcion cambiar a rojo se seleciona el elemento html de tipo caja de texto 
+    // y se le cambia el color a rojo cuando se llama a la funcion
+    function cambiarRojo(){document.getElementById('caja').style.backgroundColor="red"};
+    // funcion cambiar a rojo se seleciona el elemento html de tipo caja de texto 
+    // y se le cambia el color a azul cuando se llama a la funcion
+    function cambiarAzul(){document.getElementById('caja').style.backgroundColor="blue"};// funcion cambiar a azul
+    
